1aa7e2b0244994b61f47b639363ba084a17c20f7
Jamie Strange <jamie.strange@unosquare.com>
2019-07-22 15:34:09 +0100

    adding webpack config files, test and babel config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import greeting from './greet'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '@babel/polyfill'
+import App from './App'
+import './styles.css'
 
-console.log('Hello World')
+ReactDOM.render(<App/>, document.getElementById('app'))
