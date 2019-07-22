0d852fd27e092af81bcb45df673fc3f2f8219f11
Jamie Strange <jamie.strange@unosquare.com>
2019-07-22 16:20:32 +0100

    added prettier, eslint

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
 import React from 'react'
-import {hot} from 'react-hot-loader'
+import { hot } from 'react-hot-loader'
 
 const Warning = React.lazy(() => import('./Warning'))
 
 class App extends React.Component {
-    state = {
-        count: 0
-    }
+  state = {
+    count: 0
+  }
 
-    increment = () => {
-        this.setState(state => ({count: state.count + 1}))
-    }
+  increment = () => {
+    this.setState(state => ({ count: state.count + 1 }))
+  }
 
-    decrement = () => {
-        this.setState(state => ({count: state.count - 1}))
-    }
+  decrement = () => {
+    this.setState(state => ({ count: state.count - 1 }))
+  }
 
-    render() {
-        const {count} = this.state
-        return (
-        <div>
-            <h1>Hello world!!</h1>
-            <h2 className={count > 10 ? 'warning' : null}>
-                Count: {this.state.count}
-            </h2>
-            <button onClick={this.increment}>+</button>
-            <button onClick={this.decrement}>-</button>
-            {count > 10 ? 
-            <React.Suspense fallback={null}>
-                <Warning/>
-            </React.Suspense> 
-            : null}
-        </div>
-        )
-    }
+  render() {
+    const { count } = this.state
+    return (
+      <div>
+        <h1>Hello world!!</h1>
+        <h2 className={count > 10 ? 'warning' : null}>
+          Count: {this.state.count}
+        </h2>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+        {count > 10 ? (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        ) : null}
+      </div>
+    )
+  }
 }
 
 export default hot(module)(App)
